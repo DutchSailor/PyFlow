a7a4a72dc05cb14ba9624a12629bd15c5bb19133
microelly2 <thomas@freecadbuch.de>
2019-12-10 16:08:17 +0100

    callback for enumeration pin added

--- PyFlow/UI/Canvas/UINodeBase.py
+++ PyFlow/UI/Canvas/UINodeBase.py
@@ -1326,7 +1326,26 @@ class UINodeBase(QGraphicsWidget, IPropertiesViewSupport, IUINode):
                     continue
                 dataSetter = inp.call if inp.isExec() else inp.setData
                 w = createInputWidget(inp.dataType, dataSetter, inp.defaultValue(), inp.getInputWidgetVariant(), pinAnnotations=inp._rawPin.annotationDescriptionDict)
+
+
+
+
                 if w:
+
+
+                    w.pin=inp
+
+                    #hack me
+                    # add backref to pin ui object and call initializer
+                    try:
+                        w.runpin()
+                    except:
+                        pass
+                    #hack end
+
+
+
+
                     w.setToolTip(inp.description)
                     inp.dataBeenSet.connect(w.setWidgetValueNoSignals)
                     w.blockWidgetSignals(True)
